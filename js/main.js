ba646fd692c4860ea3136073a53da62c8f1d4db1
FrancescoCapriolo <francesco.capriolo92@gmail.com>
2022-03-20 23:50:01 +0100

    corretto

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ let createNumber = [];
 document.getElementById("random-number").addEventListener('click', function () {
     numberList.innerHTML = "";
     createNumber = [];
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < 5; i++) {
         let number = Math.floor((Math.random() * 50) + 1);
         createNumber.push(number);
         numberList.innerHTML = createNumber;
